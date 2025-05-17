6106fa9d526175ec6bd346dbf7fc39f0b25a2b5c
VitorsosterF <vitor.soster.f@gmail.com>
2025-05-17 12:21:49 -0300

    feat: adição do backend e código api
    Adição do código do backend para subir no render e interagir com o front e a tag nfc no projeto.

--- app-rfid/src/App.jsx
+++ app-rfid/src/App.jsx
@@ -3,14 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Carrinho from './components/carrinho'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <Carrinho />
-    </>
+    <Routes>
+      <Route path='/' />
+      <Route path='/carrinho' />
+    </Routes>
   )
 }
 
---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.use(cors());
+app.use(express.json());
+
+const cestas = {};
+
+app.post('/api/cesta/:id', (req, res) => {
+    const { id } = req.params;
+    const { produtos } = req.body;
+    cestas[id] = produtos;
+    res.json({ mensagem: 'Cesta salva com sucesso!' });
+});
+
+app.get('/api/cesta/:id', (req, res) => {
+    const { id } =req.params;
+    if (!cestas[id]) {
+        return res.status(404).json({ erro: 'Cesta não encontrada' });
+    }
+    res.json ({ produtos: cestas[id] });
+});
+
+app.listen(port, () => {
+    console.log(`servidor rodando em http://localhost:${port}`);
+})
