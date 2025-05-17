abfcfe73472072e258d6a8b0782dee2f47a6360c
DaviRodriFv <davir2493@gmail.com>
2025-05-17 15:16:17 -0300

    commit para o teste

--- app-rfid/src/App.jsx
+++ app-rfid/src/App.jsx
@@ -1,19 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Carrinho from './components/carrinho'
-import { Route, Routes } from 'react-router-dom'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import Carrinho from './components/carrinho';
+import NFCTrigger from './components/NFCTrigger'; // ✅ Importação que faltava
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <Routes>
       <Route path='/' element={<NFCTrigger />} />
-      <Route path='/carrinho' element={<Carrinho />}/>
+      <Route path='/carrinho' element={<Carrinho />} />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
